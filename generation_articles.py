454d04a8f6fb824660f139616e47b0d37e80d3a1
Inversia <eryckov37@gmail.com>
2023-10-23 04:10:51 +0300

    Finally version

---
+++ generation_articles.py
@@ -0,0 +1,43 @@
+"""
+Комментарии здесь описывают ход моей работы, что я хотел сделать и зачем.
+Виртуальные простратсва не нужно запускать, так как с помощью Python я создам основу для работы над Заданием №1
+
+Для начала работы, нам необходимо создать виртуально пространство командой: python -m venv {any_venv_name}
+Активируем VENV: venv\Scripts\activate
+После необходимо установить библиотеку g4f: pip install g4f
+"""
+
+import g4f # Используем библиотеку для генерации статей
+
+instruction = """
+Твоя задача придумать aнoнс к любой нововсти, которую ты можешь придумать. Тебе нужно отвечать на русском больше 250 но меньше 500 символов. Не добавляй объяснения и ссылки на статью.
+"""
+def ask_gpt(instruction: str, integer: int) -> str:
+    responce = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_35_turbo_16k_0613,
+        messages=[{'role': 'user', 'content': instruction}],
+    )
+    print(responce)
+    with open('article.txt', 'a') as file:
+        print(responce + "\n", file=file)
+    print(str(f"done with {integer} article"))
+
+# for i in range(0,25):
+#     ask_gpt(instruction, i)
+# file.close()
+
+count = 0
+with open("article.txt", "r") as file:
+    for line in file: 
+        if line == '\n':
+            print(type(line))
+            continue
+        else: count+=1
+print(count)
+
+with open("article.txt", "r") as f:
+    lines = f.readlines()
+with open("article.txt", "w") as f:
+    for line in lines:
+        if line.strip("\n"):
+            f.write(line)
---
+++ main.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <head>
+        <style>
+            body{
+                background-color: #111111;
+            }
+            .cards {
+                display: flex;
+                flex-flow: column;
+                height: 300PX;
+                width: 280PX;
+                background: rgba(255, 255, 255, 0.2);
+                border-radius: 16px;
+                box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+                backdrop-filter: blur(5px);
+                -webkit-backdrop-filter: blur(5px);
+                border: 1px solid rgba(255, 255, 255, 0.3);
+                padding: 6px;
+                margin: 10px;
+            }
+            img{
+                height: 150px;
+                width: 280PX;
+            }
+            p{
+                color: white;
+                font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            }
+            a{
+                color: yellow;
+            }
+            .block{
+                display: flex;
+                flex-flow: row;
+                width: 1340px;
+                flex-wrap: wrap;
+            }
+        </style>
+    </head>
+    <body>
+        <div class = "block">
+        <?php
+        $fd = fopen("article.txt", 'r') or die("не удалось открыть файл");
+        setlocale(LC_ALL, 'Russian_Russia.1251');
+        for ($i = 0; $i < 25; $i++) {
+            $line = fgets($fd);
+            echo '<div class="cards">';
+            echo '<img src="https://proprikol.ru/wp-content/uploads/2022/10/kartinki-s-mezhdunarodnym-dnem-gor-16-scaled.jpg">';
+            echo '<p>';
+            $new_string = '';
+            echo '<p>';
+            $line_length = strlen($line);
+            for ($j = 0; $j <= 250; $j++) {
+                if ($j < $line_length) {
+                    $new_string .= $line[$j];
+                }
+                if ($j == 250) {
+                    $new_string .= '...';
+                    break;
+                }
+            }
+            $space_count = 0;
+            $link_count = 250;
+            for ($j = 250; $j <= 250; $j--) {
+                if ($new_string[$j] == ' ') {
+                    $space_count++;
+                }
+                if ($space_count == 3) {
+                    $link_count--;
+                    break;
+                }
+                $link_count--;
+            }
+            if ($link_count >= 0) {
+                echo substr($new_string, 0, $link_count + 2);
+                echo '<a href="https://google.com">' . substr($new_string, $link_count + 2, strlen($new_string)) . '</a>';
+            } else {
+                echo $new_string;
+            }
+            echo '</p>';
+            echo '</div>';
+        }
+        fclose($fd);
+        ?>
+        </div>
+    </body>
+</html>
+
+<!-- <?php
+        $fd = fopen("article.txt", 'r') or die("не удалось открыть файл");
+        for ($i = 0; $i < 3; $i++) {
+            $line = fgets($fd);
+            if($line == ' '){
+                continue;
+            }
+            else{
+                echo '<div class = "block">';
+                echo '<img src="https://proprikol.ru/wp-content/uploads/2022/10/kartinki-s-mezhdunarodnym-dnem-gor-16-scaled.jpg">';
+                echo '<p>';
+                $new_string = '';
+                for($i = 0; $i < 250; $i++){
+                    $new_string .= $line[$i];
+                    echo $new_string;
+                }
+                echo $new_string;
+                echo '</p>';
+                echo '<p>';
+                $string = $line;
+                $lastThreeWords = array_slice($words, -3);
+                $link = implode(" ", $lastThreeWords);
+                $word = str_word_count($string, 1);
+                echo $word;
+                echo $line;
+                echo '<a href = "google.ru">'.$link.'</a>';
+                echo '</p>';
+                echo '</div>';
+            }
+        }
+        fclose($fd);
+        ?> -->
